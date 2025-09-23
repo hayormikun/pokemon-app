a4953a454409f515fe0166c84b884c028b8543f8
Favour Akomolafe <45855421+hayormikun@users.noreply.github.com>
2025-09-23 19:21:17 -0400

    first commit

---
+++ src/providers/AppProvider.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 2 * 60 * 1000,
+      retry: 1,
+    },
+  },
+})
+
+export function AppProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryPurple: "#6A0DAD",
+        gold: "#FFD700",
+        primaryBlack: "#070707",
+        secondaryBlack: "#020202",
+      },
+    },
+  },
+  plugins: [],
+}; 
